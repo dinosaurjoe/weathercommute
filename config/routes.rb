066633612044b165b57db6b7d386e15d4841c448
Joseph Schafer <joe.schafer@itsmycargo.com>
2020-02-09 16:55:17 +0100

    json render

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'weather', to: 'weather_plan#index'
+  namespace :v1, defaults: { format: 'json'} do
+    get 'weather', to: 'weather_plan#index'
+  end
 end
